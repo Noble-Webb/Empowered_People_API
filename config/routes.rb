96e4a7f1712a40204628cb45281645928e3e0832
Justin_Webb <Webb.n.justin@gmail.com>
2020-12-14 16:56:43 -0600

    set-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
-  resources :comments
-  post '/auth', to: 'auth#create'
-  get '/current_user', to: 'auth#show'
-  post '/signup', to: 'user#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20201214174456_create_posts.rb
+++ db/migrate/20201214174456_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.string :author 
       t.string :title
       t.string :content
       t.integer :upvote
