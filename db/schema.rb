f223dbf3c7581e168c8aad04dd36e16949051ce0
Justin_Webb <Webb.n.justin@gmail.com>
2021-03-07 15:47:02 -0600

    add names to maps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_173939) do
+ActiveRecord::Schema.define(version: 2021_03_07_213442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_173939) do
     t.text "exits"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "posts", force: :cascade do |t|
